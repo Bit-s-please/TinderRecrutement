0b50649f9b095077b8e05719eec395bf45486126
GuillaumeBchd <guillaume.bouchard@etu.univ-tours.fr>
2019-12-05 23:46:53 +0100

    constructeur parseur XML de Profil fait

--- src/Profil.java
+++ src/Profil.java
@@ -1,22 +1,22 @@
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXNotSupportedException;
 
+import javax.imageio.ImageIO;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.awt.font.TextHitInfo;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public class Profil {
     //Attributs
     String nom;
-    LocalDate duree;
+    String duree;
     String ville;
 
     String contexte;
@@ -24,22 +24,89 @@ public class Profil {
     List<String> missions;
     List<Techno> technos;
 
+    String contact;
+
     //Constructeurs
     public Profil(String chemin){
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        try{
+            DocumentBuilder parseur = factory.newDocumentBuilder();
+            File xmlFile = new File(chemin);
+
+            Document xml = parseur.parse(xmlFile);
+            Element Offre = xml.getDocumentElement();
+
+            NodeList nodes = Offre.getChildNodes();
+
+            for (int i = 0; i < nodes.getLength(); i++){
+                Node node = nodes.item(i);
+                switch (node.getNodeName()){
+                    case "title":
+                        setNom(node.getNodeValue());
+                        break;
+                    case "duree":
+                        setDuree(node.getNodeValue());
+                        break;
+                    case "ville":
+                        setVille(node.getNodeValue());
+                        break;
+                    case "contexte":
+                        setContexte(node.getNodeValue());
+                        break;
+                    case "profil":
+                        addProfilRecherche(node.getNodeValue());
+                        break;
+                    case "missions":
+                        addMission(node.getNodeValue());
+                        break;
+                    case "technos":
+                        NodeList technos = node.getChildNodes();
+                        Techno techno = new Techno();
+                        for (int j = 0; j<technos.getLength(); j++){
+                            Node n = technos.item(j);
+                            if (n.getNodeName() == "nom") {
+                                techno.setNom(n.getNodeValue());
+                            }
+                            if (n.getNodeName() == "image") {
+                                Image logo = ImageIO.read(new File(n.getNodeValue()));
+                                techno.setLogo(logo);
+                            }
+                        }
+                        break;
+                    case "contact":
+                        setContact(node.getNodeValue());
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+
+        }catch (ParserConfigurationException p){
+            p.printStackTrace();
+        }catch (SAXException s){
+            s.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
     //Getters et setters
     public String getNom(){return nom;}
-    public LocalDate getDuree(){return duree;}
+    public String getDuree(){return duree;}
     public String getVille(){return ville;}
 
     public String getContexte(){return contexte;}
     public String getProfilRecherche(int index){return profilRecherche.get(index);}
     public String getMission(int index){return missions.get(index);}
     public Techno getTechno(int index){return technos.get(index);}
+    public String getContact() {return contact; }
 
     public void setNom(String nom){this.nom = nom;}
-    public void setDuree(LocalDate duree){this.duree = duree;}
+    public void setDuree(String duree){this.duree = duree;}
     public void setVille(String ville){this.ville = ville;}
 
     public void setContexte(String contexte){this.contexte = contexte;}
@@ -59,4 +126,5 @@ public class Profil {
     public void removeTechno(Techno tec){technos.remove(tec);}
     public void clearTechno(){technos.clear();}
 
+    public void setContact(String contact) {this.contact = contact; }
 }
--- src/Techno.java
+++ src/Techno.java
@@ -1,6 +1,24 @@
 import java.awt.*;
 
 public class Techno {
+    //Attributs
     String nom;
     Image logo;
+
+    //Constructeurs
+
+    //Getters et setters
+    public String getNom() {
+        return nom;
+    }
+    public Image getLogo() {
+        return logo;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setLogo(Image logo) {
+        this.logo = logo;
+    }
 }
